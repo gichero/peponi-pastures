8aada2cc7799bdff8b6145e8d6a244f59eba3084
Amos Gichero <amosgichero@gmail.com>
2021-02-08 22:31:48 -0500

    style modal

--- src/components/Cards/ImageCardItem/ImageCardItem.js
+++ src/components/Cards/ImageCardItem/ImageCardItem.js
@@ -8,15 +8,19 @@ import "react-medium-image-zoom/dist/styles.css";
 const ImageCardItem = ({ src, alt }) => {
 	return (
 		<>
-			<li className="cards__item">
-				<Link className="cards__item__link" to="#">
+			<li className="cards__item-image">
+				<Link className="cards__item__link-image" to="#">
 					<Zoom>
 						<figure className="cards__item__pic-wrap-image">
 							<img
 								src={src}
 								alt={alt}
 								className="cards__item__img"
-								width="400"
+								style={{
+									borderRadius: "2px",
+									marginTop: "1em",
+									marginBottom: "1em",
+								}}
 							/>
 						</figure>
 					</Zoom>
