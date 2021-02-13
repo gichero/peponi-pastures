d9d8334be4ae85516767048f7f75ab600a83ecf3
Amos Gichero <amosgichero@gmail.com>
2021-02-13 13:53:51 -0500

    added text to the image pages

--- src/components/Cards/ImageCards/WaterImageCards.js
+++ src/components/Cards/ImageCards/WaterImageCards.js
@@ -13,7 +13,29 @@ import water06 from "../../../images/Water/water06.jpg";
 const WaterImageCards = () => {
 	return (
 		<div className="cards">
-			<h1 className="cards__headline">Water Images</h1>
+			<h1 className="cards__headline">Water Irrigation Project</h1>
+			<div className="cards__text__wrapper">
+				<p className="cards_text">
+					Bacon ipsum dolor amet leberkas strip steak porchetta short loin.
+					Hamburger pork loin brisket t-bone. Alcatra pork spare ribs fatback,
+					chicken shankle ham tongue chislic doner ribeye shoulder brisket.
+					Salami biltong landjaeger chicken. Meatball beef ribs burgdoggen short
+					loin. Buffalo kielbasa landjaeger biltong porchetta ham swine venison
+					leberkas short ribs kevin. Shank ribeye prosciutto, rump leberkas
+					porchetta chuck. Shoulder rump flank doner turducken buffalo. Ball tip
+					corned beef beef cupim chislic sirloin kevin. Sirloin strip steak
+					bacon cupim prosciutto alcatra kielbasa. T-bone bacon porchetta,
+					tri-tip boudin strip steak filet mignon andouille. Kielbasa shankle
+					turkey, chislic ball tip tenderloin ground round brisket shoulder.
+					Tri-tip boudin kevin, t-bone pork belly alcatra ball tip. Boudin
+					biltong turkey meatloaf kielbasa. Pork belly spare ribs pork chop
+					chicken shank cupim salami jowl. Fatback chicken chuck pork chop,
+					cupim flank spare ribs sirloin. Jowl andouille venison, ham kevin
+					shankle beef ribs landjaeger chislic short ribs sirloin filet mignon
+					leberkas. Pork loin shankle pork belly landjaeger pancetta ribeye.
+					Pork chop doner jerky pig ball tip boudin venison ham ribeye buffalo.
+				</p>
+			</div>
 			<div className="cards__container">
 				<div className="cards__wrapper">
 					<ul className="cards__items">
