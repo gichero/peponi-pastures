3cd3de0f1ce0ddb198f36ad8445c820f4603f8b0
Amos Gichero <amosgichero@gmail.com>
2021-03-13 00:41:38 -0500

    add more text

--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -6,7 +6,7 @@ import "./Mission.css";
 const Mission = () => {
 	return (
 		<div className="mission-container">
-			<h3 className="mission__headline">OUR MISSION</h3>
+			<h3 className="mission__headline">Our Mission</h3>
 			<div className="mission__content-wrapper">
 				<p className="mission__content">
 					There is this perception that agriculture has not achieved its aim in
@@ -16,6 +16,14 @@ const Mission = () => {
 					have built an agribusiness that employs our women and youth, that
 					employs forward thinking and capacity building and that embraces the
 					beauty of the soil and of agriculture.
+				</p>
+				<h3 className="mission__headline">How are we moving?</h3>
+				<p className="mission__content">
+					We pride ourselves in quality and a strong ethical culture. Our labor
+					comprises 75% women from our locality and youth, we train and equip
+					them to work for us and for themselves. We have a clear focus of where
+					we are going as a business and this can only be done ethically and
+					sustainably. That there, is our mantra.
 				</p>
 			</div>
 		</div>
